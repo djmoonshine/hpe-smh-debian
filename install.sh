afbda15d7b34abdf82be35b22a34f8561755a633
root <stefan@trimmat.se>
2019-08-10 15:47:25 +0200

    Bugfixes

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-copy mcp.list /etc/apt/sources.list.d/
+cp mcp.list /etc/apt/sources.list.d/
+apt install curl
 curl http://downloads.linux.hpe.com/SDR/hpPublicKey2048.pub | apt-key add -
 curl http://downloads.linux.hpe.com/SDR/hpPublicKey1024.pub | apt-key add -
 curl http://downloads.linux.hpe.com/SDR/hpPublicKey2048_key1.pub | apt-key add -
